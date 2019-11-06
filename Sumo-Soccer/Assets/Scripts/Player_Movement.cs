0346e6a23dd38b894159e445c28c625e29a1d748
Ash-JMC <ahall1@jmc.edu.au>
2019-11-06 11:58:37 +1100

    Base Movement Scripting + Collision Zones
    - Started base movement script for players.
    - Added 4 player input to InputManager
    - Added collision mesh to Arena bounds
    - Setup 2 players to test interactions
    
    ___________________________
    NOTES:
    ___________________________
    
    Using RigidBody velocity to push the players around feel artificial and does not respond well to external forces.
    
    Consider using RigidBody addForce with some kind of speed limiter instead.

---
+++ Sumo-Soccer/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public int playerNum;
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float baseMass;
+    public Vector3 movement;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    private void GetInput()
+    {
+        float x = Input.GetAxis("X_" + playerNum);
+        float z = Input.GetAxis("Z_" + playerNum);
+        movement = Vector3.ClampMagnitude(new Vector3(x, 0, z) * moveSpeed, moveSpeed);
+    }
+    private void Move()
+    {
+        rb.velocity = movement;
+
+        if(movement.magnitude > 1f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(rb.velocity, Vector3.up);
+            Quaternion lookRot = Quaternion.RotateTowards(transform.rotation, targetRot, rotateSpeed * Time.deltaTime);
+            transform.rotation = lookRot;
+        }
+        
+    }
+}
